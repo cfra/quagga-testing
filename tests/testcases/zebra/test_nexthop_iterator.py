b5db06099ac5f66ad57d3d5b786978788bb8ce82
Christian Franke <nobody@nowhere.ws>
2014-05-27 22:23:22 +0100

    tests: nexthop iteration test might take longer that default timeout

--- tests/testcases/zebra/test_nexthop_iterator.py
+++ tests/testcases/zebra/test_nexthop_iterator.py
@@ -3,5 +3,5 @@ from testutils import expect
 def run_test():
     program = expect.spawn("./testcases/zebra/test_nexthop_iterator")
     yield program.expect('Simple test passed.\r\n', 'Simple Test')
-    yield program.expect('PRNG test passed.\r\n', 'PRNG Test')
+    yield program.expect('PRNG test passed.\r\n', 'PRNG Test', timeout=20)
     yield program.finish()
